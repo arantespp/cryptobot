25653774433aa9cd1d2c9f0186dfed18aff27638
Pedro Arantes <arantespp@gmail.com>
2021-11-21 18:34:02 -0300

    fix: add tests before deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
       name: 'CryptoBot',
       script: 'dist/index.js',
       watch: ['dist/index.js'],
-      watch_delay: 1000,
+      watch_delay: 30 * 1000,
       time: true,
       env_production: {
         NODE_ENV: 'production',
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,6 @@
 SERVER_IP=54.161.159.172
 
+yarn run test
 yarn run build
 yarn run release
 scp -i ~/.ssh/arantespp-personal-aws.pem -r ./dist/* ubuntu@$SERVER_IP:~/cryptobot/dist/
