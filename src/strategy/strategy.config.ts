5460624cb5c70f6d8aca8eebc8c1905cb330aff5
Pedro Arantes <arantespp@gmail.com>
2021-12-02 05:03:11 -0300

    fix: LOWEST_QUANTITY_ASSETS_TO_NOT_TRADE

--- src/strategy/strategy.config.ts
+++ src/strategy/strategy.config.ts
@@ -15,10 +15,10 @@ export const MIN_NOTIONAL_MULTIPLIER = 1.5;
 export const MIN_NOTIONAL_TO_TRADE = 10;
 
 /**
- * Trade only a 25% of the wallet.
+ * Trade only a 30% of the wallet.
  */
 export const LOWEST_QUANTITY_ASSETS_TO_NOT_TRADE = Math.round(
-  Object.keys(WALLET).length * 0.2
+  Object.keys(WALLET).length * 0.7
 );
 
 /**
